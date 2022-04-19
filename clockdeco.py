7aca2c3826cb37d62c80686395f7bc2a4830e1de
xuexi1126 <847305282@qq.com>
2022-04-19 20:42:06 +0800

    协程

--- clockdeco.py
+++ clockdeco.py
@@ -2,6 +2,8 @@ from ast import arg
 from cgitb import reset
 import time
 
+from coroutil.coroutil import coroutine
+
 
 def clock(func):
     def clocked(*args):
@@ -12,4 +14,25 @@ def clock(func):
         arg_str = ','.join(repr(arg) for arg in args)
         print('[%0.8fs]%s(%s)->%r' % (elapsed, name, arg_str, result))
         return result
+
     return clocked
+
+
+@coroutine
+def averager():
+    total = 0.0
+    count = 0
+    average = None
+    while True:
+        print('-------------------------')
+        term = yield average
+        total += term
+        count += 1
+        average = total / count
+
+
+if __name__ == "__main__":
+    coro_avg = averager()
+    # next(coro_avg)
+    print(coro_avg.send(10))
+    print(coro_avg.send(30))
--- setence/sentence_gen.py
+++ setence/sentence_gen.py
@@ -7,13 +7,17 @@ RE_WORD = re.compile('\w+')
 class Sentence:
     def __init__(self, text):
         self.text = text
-        self.words = RE_WORD.findall(text)
+        # self.words = RE_WORD.findall(text)
 
     def __repr__(self):
         return 'Sentence(%s)' % reprlib.repr(self.text)
 
-    def __iter__(self):
-        for word in self.words:
-            yield word
-        return
+    # def __iter__(self):
+    #     for word in self.words:
+    #         yield word
+    #     return
 
+    def __iter__(self):
+        # for match in RE_WORD.finditer(self.text):
+        #     yield match.group()
+        return (match.group() for match in RE_WORD.finditer(self.text))
