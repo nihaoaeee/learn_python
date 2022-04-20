cc96965210de7c7386ea199a1a5c0c88459fa1fe
xuexi1126 <847305282@qq.com>
2022-04-20 20:17:40 +0800

    协程

--- future_demo/flags_threadpool.py
+++ future_demo/flags_threadpool.py
@@ -11,11 +11,29 @@ def download_on(cc):
     return cc
 
 
-def download_many(cc_list):
+def download_many1(cc_list):
     workers = min(MAX_WORKERS, len(cc_list))
     with futures.ThreadPoolExecutor(workers) as executor:
         res = executor.map(download_on, sorted(cc_list))
     return len(list(res))
+
+
+def download_many(cc_list):
+    cc_list = cc_list[:5]
+    with futures.ThreadPoolExecutor(max_workers=3) as executor:
+        to_do = []
+        for cc in sorted(cc_list):
+            future = executor.submit(download_on, cc)
+            to_do.append(future)
+            msg = 'Scheduled for {}:{}'
+            print(msg.format(cc, future))
+        results = []
+        for future in futures.as_completed(to_do):
+            res = future.result()
+            msg = '{} result: {!r}'
+            print(msg.format(future, res))
+            results.append(res)
+    return len(res)
 
 
 if __name__ == "__main__":
