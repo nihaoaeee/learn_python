a69a8965ecf032475c16ba98790692400e5def32
xuexi1126 <15510262290@163.com>
2022-09-28 15:20:50 +0800

    yield from 例子

--- yield_learn/average_yield.py
+++ yield_learn/average_yield.py
@@ -30,12 +30,41 @@ def averager1():
         total += term
         count += 1
         average = total / count
-
     return Result(count, average)
 
 
+def grouper(results, key):
+    while True:
+        results[key] = yield from averager1()
+
+
+def main(data):
+    results = {}
+    for key, values in data.items():
+        group = grouper(results, key)
+        next(group)
+        for value in values:
+            group.send(value)
+        group.send(None)
+
+    print(results)
+    report(results)
+
+
+def report(results):
+    for key, result in sorted(results.items()):
+        group, unit = key.split(';')
+        print('{:2} {:5} averaging {:.2f}{}'.format(result.count, group, result.average, unit))
+
+
+data = {
+    "girls;kg": [1, 2, 3, 4, 5, 6, 7, 8, 9]
+}
+
 if __name__ == "__main__":
-    coro_average = average_yield()
-    for i_ in range(5):
-        res = coro_average.send(5 * i_)
-        print(res)
+    # coro_average = average_yield()
+    # for i_ in range(5):
+    #     res = coro_average.send(5 * i_)
+    #     print(res)
+
+    main(data)
